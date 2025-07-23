49b36f013f1bf730b0624057b4a921e958107c08
wees-s <wesleyalexandre3946@gmail.com>
2025-07-23 18:12:54 -0300

    updates

--- ft_split.c
+++ ft_split.c
@@ -6,66 +6,86 @@
 /*   By: wedos-sa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 13:28:36 by wedos-sa          #+#    #+#             */
-/*   Updated: 2025/07/23 17:08:37 by wedos-sa         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:12:28 by wedos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
-size_t	count_words(char const *s, char c)
+size_t    count_words(char const *s, char c)
 {
-	size_t	i;
-	size_t	j;
-	int	flag;
+    size_t    i;
+    size_t    j;
+    int    flag;
 
-	flag = 1;
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if ((s[i] != c) || (s[i] == c && s[i + 1] != c))
-			j++;
-			i++;
-		while (flag == 1 && s[i] != '\0')
-		{
-			if (s[i] == c)
-				flag = 0;
-			i++;
-		}
-		if (flag == 0 && s[i] != '\0')
-			flag = 1;
-	}
-	if (s[i] == c)
-		j = j - 1;
-	return (j);
+    flag = 1;
+    i = 0;
+    j = 0;
+    while (s[i])
+    {
+        if ((s[i] != c) || (s[i] == c && s[i + 1] != c))
+            j++;
+            i++;
+        while (flag == 1 && s[i] != '\0')
+        {
+            if (s[i] == c)
+                flag = 0;
+            i++;
+        }
+        if (flag == 0 && s[i] != '\0')
+            flag = 1;
+    }
+    if (s[i] == c)
+        j = j - 1;
+    return (j);
 }
 
-char	*word(size_t first, size_t last, char const *s)
+char    *word(size_t first, size_t last, char const *s)
 {
-	char	*string;
-	size_t	size;
+    char    *string;
+    size_t    size;
 
-	size = last - first;
-	string = (char *)malloc(size * sizeof(char));
-	if (!string)
-		return (NULL);
-	while (first < last)
-		*string++ = *s++;
-	*string++ = '\0';
-	return (word);
+    size = last - first + 1;
+    string = (char *)malloc(size * (sizeof(char)) + 1);
+    if (!string)
+        return (NULL);
+    while (first != (last + 1))
+    {
+        *string++ = s[first];
+        first++;
+    }
+    *string = '\0';
+    return (word);
 }
 
 char **ft_split(char const *s, char c)
 {
-	char *end = "$"; //APENAS PARA N DER CRASH
-	char	**res;
-	res = malloc((count_words(s, c) + 1) * sizeof(char *));
-	res[i] = word(1, 2, "abacaxi");
-	return 0;
+        char    **res;
+        size_t  size = count_words(s, c);
+        size_t  i;
+        size_t  j;
+        int     k;
+
+        res = malloc((size + 1) * sizeof(char *));
+        i = 0;
+        j = 0;
+        k = 0;
+        while (i < strlen(s))
+        {
+                if (s[i] != c)
+                        j = i;
+                while (s[i] != c && s[i] != '\0')
+                        i++;
+                res[k] = word(j, i, s);
+                i++;
+                k++;
+        }
+        res[k] = NULL;
+        return (res);
 }
 
 int main()//teste
 {
-	ft_split("aa,bb,cc,dd", ',');
-	return 0;
+        ft_split("aa,bb,cc,dd", ',');
+        return 0;
 }
